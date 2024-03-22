539a580d479d831a0d8325b5afb87985b5600371
ZalgoDev <87801213+ZalgoDev@users.noreply.github.com>
2024-03-22 12:59:24 +0700

    Update __init__.py

--- zerosharp_py/__init__.py
+++ zerosharp_py/__init__.py
@@ -1,1 +1,14 @@
+def math(num1, num2, action): 
+  if action == "add":
+    return num1 + num2
+  if action == "subtract":
+    return num1 + num2
+  if action == "multiply":
+    return num1 + num2
+  if action == "divide":
+    return num1 + num2
 
+def terminal(text):
+  print(text)
+
+terminal(math(5, 2, "divide"))
